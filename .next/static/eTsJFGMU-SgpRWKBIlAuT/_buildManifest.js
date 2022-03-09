8d940a62a7cd87ad4b8472ebc2656d684624370e
unknown <haymeemotoh22@gmail.com>
2022-03-09 19:20:20 +0800

    Added attribution

---
+++ .next/static/eTsJFGMU-SgpRWKBIlAuT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/164-00b235b10bd80cb4.js","static/chunks/pages/index-a197e772a90c3975.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- src/pages/index.js
+++ src/pages/index.js
@@ -263,6 +263,10 @@ const Index = () => {
           </div>
         </div>
       </footer>
+      <div className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
+        Coded by <a href="https://jamesromero-homepage.vercel.app/" target="_blank" rel="noreferrer">James Romero</a>.
+      </div>
 
     </>
   )
